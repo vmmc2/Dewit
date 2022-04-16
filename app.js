98a55906e4d34407d9477a5b0df8b54870516f7e
Victor Miguel <vmmc2@cin.ufpe.br>
2022-04-16 16:33:57 -0300

    Changing some things in the back-end.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.set("view engine", "ejs");
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
 
-
+/* ------------------------------- Schemas ------------------------------ */
 const taskItemSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -29,9 +29,43 @@ const taskGroupSchema = new mongoose.Schema({
     tasks: [taskItemSchema]
 });
 
+/* -------------------------------- Models ------------------------------ */
 const TaskItem = mongoose.model("TaskItem", taskItemSchema);
 
 const TaskGroup = mongoose.model("TaskGroup", taskGroupSchema);
+
+
+/* ----------------------------- Documents ------------------------------ */
+const taskItem1 = new TaskItem({
+    name: "Welcome to DewIt! Here you can keep track of your daily tasks!"
+});
+
+const taskItem2 = new TaskItem({
+    name: "You can add a new task to your list by giving it a name, putting it inside a task group and then hitting the + button in the form below!"
+});
+
+const taskItem3 = new TaskItem({
+    name: "When you are done with a task and want to delete it, all you have to do is hit the X button inside the task group section!"
+});
+
+const defaultTaskItems = [taskItem1, taskItem2, taskItem3];
+
+const defaultTaskGroup = new TaskGroup({
+    name: "Default Task Group",
+    tasks: defaultTaskItems
+});
+
+/*TaskItem.insertMany(defaultTaskItems, function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Successfully inserted the default docume")
+    }
+});
+
+defaultTaskGroup.save();*/
+
+
 
 
 
@@ -48,9 +82,13 @@ app.get("/", function(req, res){
     };
     var date = new Date();
     var currentDay = date.toLocaleDateString("en-US", options);
+    let taskGroups;
 
-    console.log(tasks);
-    res.render("list", {currentDay: currentDay, tasks: tasks});
+    TaskGroup.find({}, function(err, foundItems){
+        taskGroups = foundItems;
+        //console.log("The sent taskGroups is: " + taskGroups);
+        res.render("list", {currentDay: currentDay, taskGroups: taskGroups});
+    });
 });
 
 app.post("/", function(req, res){
